38b3ca81de0e5390318b3ee7d2d173664d88f1c4
d_kor <akkormsk@gmail.com>
2021-10-08 10:21:22 +0300

    all tasks complete

--- Korolev_Dmitry_DZ_1/Task_2.py
+++ Korolev_Dmitry_DZ_1/Task_2.py
@@ -1,21 +1,30 @@
 def cube_str():
-    return [n**3 for n in range(1, 1000, 2)]
+    return [n ** 3 for n in range(1, 1000, 2)]
+
 
 def summ_dev_7(my_str):
-    summ = 0
+    sum_num = 0
     for num in my_str:
-        num_summ = sum([int(x) for x in str(num)])
-        term = num_summ if num_summ % 7 == 0 else 0
-        summ += term
-    return summ
+        sum_i = 0
+        for i in str(num):
+            sum_i += int(i)
+        if sum_i % 7 == 0:
+            sum_num += sum_i
+    return sum_num
 
-def str_plus(my_str):
-    for i, num in enumerate(my_str):
+
+def summ_dev_7_mod(my_str):
+    sum_num = 0
+    for num in my_str:
         num += 17
-        my_str[i] = num
-    return my_str
+        sum_i = 0
+        for i in str(num):
+            sum_i += int(i)
+        if sum_i % 7 == 0:
+            sum_num += sum_i
+    return sum_num
 
-# print(cube_str())
-# print(summ_dev_7(cube_str()))
-# print(summ_dev_7(str_plus(cube_str())))
 
+print(cube_str())
+print(summ_dev_7(cube_str()))
+print(summ_dev_7_mod(cube_str()))
