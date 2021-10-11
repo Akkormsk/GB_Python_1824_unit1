f16401553b937b76ae5ca9f96d95e1c047674c3a
d_kor <akkormsk@gmail.com>
2021-10-11 17:43:29 +0300

    Выполнил задание 1

---
+++ Korolev_Dmitry_DZ_3/Task_1.py
@@ -0,0 +1,17 @@
+dictionary = {
+    'one' : 'один',
+    'two' : 'два',
+    'three' : 'три',
+    'four' : 'четыре',
+    'five' : 'пять',
+    'six' : 'шесть',
+    'seven' : 'семь',
+    'eight' : 'восемь',
+    'nine' : 'девять',
+    'ten' : 'десять',
+}
+
+def num_translate(num):
+    return dictionary.get(num)
+
+print(num_translate('six'))
