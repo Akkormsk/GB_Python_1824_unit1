97b4295bdd176ae882d2746ca3f9553b7df299ec
d_kor <akkormsk@gmail.com>
2021-10-11 10:59:48 +0300

    Выполнено задание 2, но с костылями и не универсально

---
+++ Korolev_Dmitry_DZ_2/Task_2.py
@@ -0,0 +1,15 @@
+import re
+task_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
+n=0
+for i in enumerate(task_list[:]):
+    obj = re.search('\d+', i[1])
+    if obj:
+        if len(obj.group()) == 1:
+            num_list = list(task_list[i[0]+n])
+            num_list.insert(-1, '0')
+            task_list[i[0]+n] = ''.join(num_list)
+        task_list.insert(i[0]+n, '"')
+        task_list.insert(i[0]+2+n, '"')
+        n = n+2
+
+print(' '.join(task_list))
