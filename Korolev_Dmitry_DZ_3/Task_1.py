e84e1b584961f9cf4df777f603ab19fc23b6a554
d_kor <akkormsk@gmail.com>
2021-10-12 02:42:50 +0300

    Выполнил задания 3 и 4

--- Korolev_Dmitry_DZ_3/Task_1.py
+++ Korolev_Dmitry_DZ_3/Task_1.py
@@ -1,18 +1,20 @@
 dictionary = {
-    'one' : 'один',
-    'two' : 'два',
-    'three' : 'три',
-    'four' : 'четыре',
-    'five' : 'пять',
-    'six' : 'шесть',
-    'seven' : 'семь',
-    'eight' : 'восемь',
-    'nine' : 'девять',
-    'ten' : 'десять',
+    'one': 'один',
+    'two': 'два',
+    'three': 'три',
+    'four': 'четыре',
+    'five': 'пять',
+    'six': 'шесть',
+    'seven': 'семь',
+    'eight': 'восемь',
+    'nine': 'девять',
+    'ten': 'десять',
 }
+
 
 def num_translate_adv(num):
     res = str(dictionary.get(num.lower())).capitalize() if num.istitle() else dictionary.get(num)
     return res
+
 
 print(num_translate_adv('Six'))
