d9ebae7e57910212d84119cc75b5aeb911796057
d_kor <akkormsk@gmail.com>
2021-10-29 18:41:30 +0300

    Сделал задание 4 и 5

---
+++ Korolev_Dmitry_DZ_6/Task_4.py
@@ -0,0 +1,35 @@
+import json
+from itertools import zip_longest
+from sys import exit
+names = []
+names_2 = []
+names_3 = []
+hobs = []
+my_list = ['Имена', 'Фамилии', 'Отчества', 'Увлечения']
+list_list=[]
+with open('users.csv', 'r', encoding='utf-8') as f_1:
+    with open('hobby.csv', 'r', encoding='utf-8') as f_2:
+        if sum(1 for i in f_2) > sum(1 for j in f_1):
+            exit(1)
+        f_1.seek(0)
+        f_2.seek(0)
+        for line in f_1:
+            name = line.split(',')
+            names.append(name[1])
+            names_2.append(name[0])
+            names_3.append(name[2].strip())
+        list_list.append(names)
+        list_list.append(names_2)
+        list_list.append(names_3)
+        for line in f_2:
+            hob = map(lambda s: s.strip(), line.split(','))
+            hobs.extend(hob)
+        list_list.append(hobs)
+        res_dict = {k: v for k, v in zip_longest(my_list, list_list)}
+
+with open('dict_file_4.txt', 'w', encoding='utf-8') as f_3:
+    json.dump(res_dict, f_3)
+
+with open('dict_file_4.txt', 'r', encoding='utf-8') as f_3:
+    dict_test = json.load(f_3)
+    print(dict_test)
