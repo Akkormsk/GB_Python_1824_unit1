d45d611bf47bf831ed99b988db8bcf8085bf7fb4
d_kor <akkormsk@gmail.com>
2021-10-31 12:39:51 +0300

    Дополнил задание 4 с помощью DOM

--- Korolev_Dmitry_DZ_4/Task_5.py
+++ Korolev_Dmitry_DZ_4/Task_5.py
@@ -1,5 +1,6 @@
-from Utils import currency_rates
+from Utils import currency_rates, currency_rates_dom
 
 if __name__ == "__main__":
     import sys
     currency_rates(sys.argv[1])
+    currency_rates_dom(sys.argv[1])
--- Korolev_Dmitry_DZ_4/Utils.py
+++ Korolev_Dmitry_DZ_4/Utils.py
@@ -1,10 +1,14 @@
 from requests import get, utils
 from re import sub, search
-from datetime import date
+from datetime import date, datetime
+import urllib.request
+import xml.dom.minidom as minidom
+
+xml_url = 'http://www.cbr.ru/scripts/XML_daily.asp'
 
 
 def currency_rates(curr):
-    response = get('http://www.cbr.ru/scripts/XML_daily.asp')
+    response = get(xml_url)
     encodings = utils.get_encoding_from_headers(response.headers)
     content = response.content.decode(encoding=encodings)
     text_list = content.split('<Valute')
@@ -27,5 +31,30 @@ def currency_rates(curr):
         print(f'Курс {curr.upper()} к рублю: {res}')
         return
 
+
+def currency_rates_dom(curr):
+    xml_content = urllib.request.urlopen(xml_url).read()
+    dom = minidom.parseString(xml_content)
+    dom.normalize()
+    elements = dom.getElementsByTagName('Valute')
+    cur_dict = {}
+    for node in elements:
+        for child in node.childNodes:
+            if child.nodeType == 1:
+                if child.tagName == 'Value':
+                    if child.firstChild.nodeType == 3:
+                        value = float(child.firstChild.data.replace(',', '.'))
+                if child.tagName == 'CharCode':
+                    if child.firstChild.nodeType == 3:
+                        char_code = child.firstChild.data.replace(',', '.')
+        cur_dict[char_code] = value
+    res = cur_dict.get(curr.upper())
+    print(f'Данные на дату {datetime.now().date()}')
+    print(f'Курс {curr.upper()} к рублю: {res}')
+    return
+
+
 if __name__ == '__main__':
-    currency_rates(input('(*) Введите код валюты на транслите: '))
+    url = 'http://www.cbr.ru/scripts/XML_daily.asp'
+    currency_rates(input('Введите код валюты на транслите: '), url)
+    currency_rates_dom(input('Введите код валюты на транслите: '), url)
