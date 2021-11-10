bdd473f400160034f193b13f0568a4279115299a
d_kor <akkormsk@gmail.com>
2021-11-10 23:06:24 +0300

    Полностью переосмыслил задание 5

--- Korolev_Dmitry_DZ_7/Task_5.py
+++ Korolev_Dmitry_DZ_7/Task_5.py
@@ -1,31 +1,22 @@
-import json
+import pickle
 import os
 from Task_4 import new_fold
+from collections import defaultdict
 
 
-def fold_summary_write(folder):
-    d = {k: 0 for k in ['100', '1000', '10000', '100000']}
-    n_100 = n_1000 = n_10000 = n_100000 = 0
-    set_list = [set() for _ in range(4)]
-    for f in os.scandir(folder):
-        if os.stat(f).st_size in range(101):
-            set_list[0].add(f.name.split(".")[-1])
-            n_100 += 1
-            d['100'] = (n_100, list(set_list[0]))
-        elif os.stat(f).st_size in range(101, 1001):
-            set_list[1].add(f.name.split(".")[-1])
-            n_1000 += 1
-            d['1000'] = (n_1000, list(set_list[1]))
-        elif os.stat(f).st_size in range(1001, 10001):
-            set_list[2].add(f.name.split(".")[-1])
-            n_10000 += 1
-            d['10000'] = (n_10000, list(set_list[2]))
-        else:
-            set_list[3].add(f.name.split(".")[-1])
-            n_100000 += 1
-            d['100000'] = (n_100000, list(set_list[3]))
-    with open('some_data_summary.json', 'w', encoding='utf-8') as f:
-        json.dump(d, f)
+def fold_summary_write(fold):
+    st = (1, 101, 1001, 10001, 100001)
+    d = defaultdict(list)
+    for m in range(1, len(st)):
+        k = sum(1 for file in os.scandir(fold) if os.stat(file).st_size in range(st[m - 1], st[m]))
+        v = list(
+            {file.name.split(".")[-1] for file in os.scandir(fold) if os.stat(file).st_size in range(st[m - 1], st[m])})
+        d[k] = v
+    st_mod = [n - 1 for n in st[1:]]
+    d_res = dict(zip(st_mod, d.items()))
+    # print(d_res)
+    with open('some_data_summary.json', 'wb') as f:
+        pickle.dump(d_res, f)
 
 
 if __name__ == '__main__':
