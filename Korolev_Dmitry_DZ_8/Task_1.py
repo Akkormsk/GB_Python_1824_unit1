deae221fc904e310d8a273f4e006d6fd2b862dcc
d_kor <akkormsk@gmail.com>
2021-11-14 13:07:58 +0300

    Исправил задание 1

--- Korolev_Dmitry_DZ_8/Task_1.py
+++ Korolev_Dmitry_DZ_8/Task_1.py
@@ -3,9 +3,10 @@ import re
 
 def email_parse(email_adress):
     d = {}
-    re_list = re.compile("^(\w+)[@]{1}(\w+[/.]{1}\w+)$", re.IGNORECASE).findall(email_adress)
+    re_list = re.compile("^(.+)[@]{1}(\w+[/.]{1}\w+)$", re.IGNORECASE).findall(email_adress)
     if re_list:
-        d[re_list[0][0]] = re_list[0][1]
+        d['username'] = re_list[0][0]
+        d['domain'] = re_list[0][1]
     else:
         raise ValueError(f'Wrong email: {email_adress}')
     return d
