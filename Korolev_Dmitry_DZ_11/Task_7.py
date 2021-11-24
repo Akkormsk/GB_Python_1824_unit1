1ad2069a93fdc47c32583ea4bd541d8deadebc55
d_kor <akkormsk@gmail.com>
2021-11-24 11:28:51 +0300

    Задание 7

---
+++ Korolev_Dmitry_DZ_11/Task_7.py
@@ -0,0 +1,34 @@
+class Complex:
+    def __init__(self, re, im):
+        self.re = re
+        self.im = im
+
+    def __add__(self, other):
+        return Complex(self.re + other.re, self.im + other.im)
+
+    def __sub__(self, other):
+        return Complex(self.re - other.re, self.im - other.im)
+
+    def __mul__(self, other):
+        return Complex((self.re * other.re) - (self.im * other.im), (self.im * other.re) + (self.re * other.im))
+
+    def __truediv__(self, other):
+        a = self.re
+        b = self.im
+        c = other.re
+        d = other.im
+        return Complex((a*c + b*d)/(c**2 + d**2),(b*c - a*d)/(c**2 + d**2))
+
+    def __str__(self):
+        return f'{self.re}+{self.im}j' if self.im > 0 else f'{self.re}{self.im}j'
+
+
+a = Complex(3, 4)
+b = Complex(4, -5)
+print(a + b)
+print(a * b)
+print(a / b)
+
+# c = 3 + 4j
+# d = 4 - 5j
+# # print(c/d)
